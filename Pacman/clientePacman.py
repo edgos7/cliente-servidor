d85bb64e50c407f278a0a1e786c8966926ab48be
Efren Dario Gonzalez Suarez <edgos7@gmail.com>
2018-09-04 20:11:14 -0500

    figuras moviéndose con teclas

--- Pacman/clientePacman.py
+++ Pacman/clientePacman.py
@@ -4,12 +4,14 @@ import pygame
 from pygame.locals import *
 
 def mostrarImagenes (ventana,posicionPersonaje):
+	fondo=pygame.image.load("imagenes/fondo-negro.jpg")
 	ceresa=pygame.image.load("imagenes/ceresa.png")
 	pacman=pygame.image.load("imagenes/pacman.png")
 	amarillo=pygame.image.load("imagenes/amarillo.png")
 	azul=pygame.image.load("imagenes/asul.png")
 	rojo=pygame.image.load("imagenes/rojo.png")
 	rosa=pygame.image.load("imagenes/rosa.png")
+	ventana.blit(fondo, (0,0))
 	ventana.blit(ceresa, posicionPersonaje[b"ceresa"])
 	ventana.blit(pacman, posicionPersonaje[b"pacman"])
 	ventana.blit(amarillo, posicionPersonaje[b"fantasmaAmarillo"])
@@ -29,26 +31,22 @@ def main():
 	identidad = sys.argv[1].encode('ascii')
 	socket.identity = identidad
 	socket.connect("tcp://localhost:5555")
-	poller = zmq.Poller()
-	poller.register(sys.stdin, zmq.POLLIN)
-	poller.register(socket, zmq.POLLIN)
 	socket.send_multipart([b"nuevoJugador", identidad])
 	posicionPersonaje = {b"pacman":(500,350),b"fantasmaAmarillo":(0,0),b"fantasmaAzul":(0,650),b"fantasmaRojo":(950,0),b"fantasmaRosa":(950,650),b"ceresa":(300,300)}
-	ventana=""
+	alto=700
+	ancho=1000
+	pygame.init()
+	ventana=pygame.display.set_mode((ancho, alto))
+	pygame.display.set_caption("figuras de pacman moviendose")
+	
 	while True:
-		socks = dict(poller.poll())
-		if socket in socks:
-			operacion, *mensaje = socket.recv_multipart()
+		try:
+			operacion, *mensaje = socket.recv_multipart(zmq.NOBLOCK)
 			if operacion == b"personaje":
 				miPersonaje=mensaje[0]
 				print("tu personaje es: " + miPersonaje.decode("ascii"))
 			if operacion == b"iniciarJuego":
 				iniciarJuego = True
-				alto=700
-				ancho=1000
-				pygame.init()
-				ventana=pygame.display.set_mode((ancho, alto))
-				pygame.display.set_caption("figuras de pacman moviendose")
 				mostrarImagenes(ventana,posicionPersonaje)
 			if operacion == b"actualizarPosicion":
 				personaje = mensaje[0]
@@ -62,18 +60,21 @@ def main():
 			if operacion == b"pierdePacman":
 				print("los ganadores son los fantasmas")
 				exit()
+		except zmq.ZMQError as e:
+			pass
 
-		elif sys.stdin.fileno() in socks and iniciarJuego:
-			command = input()
-			if command == "d":
-				socket.send_multipart([b"movimiento", b"derecha"])
-			if command == "a":
-				socket.send_multipart([b"movimiento", b"izquierda"])
-			if command == "w":
-				socket.send_multipart([b"movimiento", b"arriba"])
-			if command == "s":
-				socket.send_multipart([b"movimiento", b"abajo"])
-
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				exit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_UP:
+					socket.send_multipart([b"movimiento", b"arriba"])
+				if event.key== pygame.K_DOWN:
+					socket.send_multipart([b"movimiento", b"abajo"])
+				if event.key== pygame.K_LEFT:
+					socket.send_multipart([b"movimiento", b"izquierda"])
+				if event.key== pygame.K_RIGHT:
+					socket.send_multipart([b"movimiento", b"derecha"])
 
 if __name__ == '__main__':
 	main()
